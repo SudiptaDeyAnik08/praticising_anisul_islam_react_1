7b7dc8aec9d37afae6b71e8650754e9dbf12cba4
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-03-18 22:12:32 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const a = "Hi this is Sudipta";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render( 
+ <div>
+   <h2>Hi</h2>
+   <h1>{a}</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, vitae.</p>
+   <p>3/18/2023</p>
+ </div>
+  
+);
