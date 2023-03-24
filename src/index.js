60d37f87769531fdb9791c4701f953feb2342a8f
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-03-24 22:07:20 +0600

    css styling

--- src/index.js
+++ src/index.js
@@ -8,15 +8,43 @@ const a = "Hi this is Sudipta";
 const currentYear = new Date().getFullYear();
 const date1 = new Date().getDate();
 const month = new Date().getMonth();
+const DateName = new Date().getDate();
+const MonthName = new Date().getMonth();
+const CurrentYear1 = new Date().getFullYear();
+
+const todoTitle = 'This is Family';
+const todoDesc = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, harum veritatis debitis officia dicta repudiandae! Quisquam cum sed molestias ea"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
+
+const HeadingStyle = {
+  backgroundColor:'purple',
+  color:"white",
+  textAlign:'center',
+  padding:'15px'
+}
+
+
+
 root.render( 
  <div>
-   <h2>Hi</h2>
-   <h1>{a}</h1>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, vitae.</p>
-   <p>{month}.{date1}.{currentYear}</p>
+   <div>
+      <h2 style={{color:'red',fontSize:'3rem'}}>Hi</h2>
+      <h1 style={HeadingStyle}>{a}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, vitae.</p>
+      <p>{month}.{date1}.{currentYear}</p>
+   </div>
+  <hr />
+   <div className='card'>
+    <h3 className='cardTitle'>{todoTitle}</h3>
+    <p className='cardDes'>{todoDesc}</p>
+    <p className='cardFooter'>{DateName + '/' + MonthName + '/' +CurrentYear1}</p>
+   </div>
+
+
  </div>
   
 );
