968563f4d360e296c148a19ffcf9cf38081947b1
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-04-23 01:51:45 +0600

    Use Effect added with dependent variable

--- src/Componets/UseEffect/UseEffect.js
+++ src/Componets/UseEffect/UseEffect.js
@@ -4,13 +4,16 @@ function UseEffect() {
     const [count,setCount] = useState(0);
     useEffect(()=>{
         console.log("UseEffect Loading");
-    })
+    },[count])
 
-
+    const handelChange =()=>{
+        const sum = count + 1;
+        setCount(sum);
+    }
   return (
     <div>UseEffect
     <h2>Count: {count}</h2>
-    <button >Count</button>
+    <button onClick={handelChange}>Count</button>
     </div>
   )
 }
