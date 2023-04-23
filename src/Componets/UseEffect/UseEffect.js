73bdd16f8cc5ce894dd9c1b1c5b2b2eb4482a50c
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-04-23 17:52:29 +0600

    learn how to use loading massage by using useEffect

--- src/Componets/UseEffect/UseEffect.js
+++ src/Componets/UseEffect/UseEffect.js
@@ -10,10 +10,37 @@ function UseEffect() {
         const sum = count + 1;
         setCount(sum);
     }
+
+
+    const [todo,setTodo] = useState(null)
+    const [isloading,setIsloading] = useState(true)
+
+    const loadingMassage = <p>Massage is loading</p>
+
+    useEffect(()=>{
+      fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(res=>{
+        return res.json();
+      })
+      .then(res=> {
+        setTodo(res);
+      setIsloading(false);
+      });
+    },[])
+
+
+
+
+
+
+
   return (
     <div>UseEffect
     <h2>Count: {count}</h2>
     <button onClick={handelChange}>Count</button>
+    {
+      isloading&& loadingMassage
+    }
     </div>
   )
 }
